64ca495285b5b3b13e296a4233eb79da6cb32497
LindseySongstad <lindsey.songstad@outlook.com>
2020-11-23 09:34:16 -0800

    Exeption Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+            Console.WriteLine("Pick a number");
+
+            try 
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number + " divided by " + input + " equals " + number / input);
+                }
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please do not deivide by zero");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Please enter a whole number");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
