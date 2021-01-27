f4529a0a95fd0c232b16f2e724450de292271785
LindseySongstad <lindsey.songstad@outlook.com>
2021-01-27 10:24:04 -0800

    Reorganizing

---
+++ Basic-C-Sharp-Exercises/Scores/Scores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\n Welcome back {uName}. Today is {date}.";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\linds\source\repos\Scores\Scores\studentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path); 
+            
+            double tScore = 0;
+
+            Console.WriteLine("\n Student Scores: \n");
+            foreach(string line in lines)
+            {
+                Console.WriteLine("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\n Total of " + lines.Length + " student scores. \t Average score: " + avgScore);   ;
+        }
+    }
+}
---
+++ Basic-C-Sharp-Exercises/Structs1/Structs1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            Console.WriteLine("Enter a number:");
+            num.amount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(num.amount);
+
+            Console.Read();
+        }
+
+        public struct Number
+        {
+            public decimal amount;
+        }
+    }
+}
