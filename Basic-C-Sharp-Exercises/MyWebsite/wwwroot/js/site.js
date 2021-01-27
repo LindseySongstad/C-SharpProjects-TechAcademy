9ca9a9a442bbfb0d37fe373fbc39e86a6e95a5cb
LindseySongstad <lindsey.songstad@outlook.com>
2021-01-27 10:31:28 -0800

    Reorganizing

---
+++ Basic-C-Sharp-Exercises/MyWebsite/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+window.onload = fadeIn;
+
+function fadeIn() {
+    var fade = document.getElementById("animate");
+    var opacity = 0;
+    var intervalID = setInterval(function () {
+
+        if (opacity < 1) {
+            opacity = opacity + 0.05
+            fade.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 100);
+}
